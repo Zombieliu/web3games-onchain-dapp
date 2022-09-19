3daf7e8e5df6eb82c53c27797160c00fa2363a87
henryliu <17600047539@163.com>
2022-09-19 14:14:39 +0800

    update

---
+++ src/components/head/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from 'next/head';
+const Heads = () =>{
+    return(
+        <>
+            <Head>
+                <title>Web3Games On-Chain | Dapp</title>
+                <link rel="icon" href="../web3gsmall.png" />
+            </Head>
+            </>
+    )
+}
+export default Heads
---
+++ src/components/skeleton/index.tsx
@@ -0,0 +1,245 @@
+
+const HiddenSkeleton = () =>{
+    return(
+        <div className="lg:flex mb-20 mx-auto">
+            <div className=" shadow p-5 rounded-md mb-10  xl:w-7/12 lg:mr-5  grid grid-cols-2 gap-4 ">
+                <div className="flex space-x-4 py-3 ">
+                    <div className="rounded-lg bg-neutral-700 h-10 w-10"></div>
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="h-2 bg-neutral-700 rounded"></div>
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="h-2 bg-neutral-700 rounded col-span-2"></div>
+                                <div className="h-2 bg-neutral-700-200 rounded col-span-1"></div>
+                            </div>
+                            <div className="h-2 bg-neutral-700 rounded"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex space-x-4 py-3 ">
+                    <div className="rounded-lg bg-neutral-700 h-10 w-10"></div>
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="h-2 bg-neutral-700 rounded"></div>
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="h-2 bg-neutral-700 rounded col-span-2"></div>
+                                <div className="h-2 bg-neutral-700-200 rounded col-span-1"></div>
+                            </div>
+                            <div className="h-2 bg-neutral-700 rounded"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex space-x-4 py-3 ">
+                    <div className="rounded-lg bg-neutral-700 h-10 w-10"></div>
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="h-2 bg-neutral-700 rounded"></div>
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="h-2 bg-neutral-700 rounded col-span-2"></div>
+                                <div className="h-2 bg-neutral-700-200 rounded col-span-1"></div>
+                            </div>
+                            <div className="h-2 bg-neutral-700 rounded"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex space-x-4  py-3">
+                    <div className="rounded-lg bg-neutral-700 h-10 w-10"></div>
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="h-2 bg-neutral-700 rounded"></div>
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="h-2 bg-neutral-700 rounded col-span-2"></div>
+                                <div className="h-2 bg-neutral-700-200 rounded col-span-1"></div>
+                            </div>
+                            <div className="h-2 bg-neutral-700 rounded"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="xl:w-5/12 ">
+                <div className="  shadow p-5 rounded-md">
+                    <div className="flex ">
+                        <div className="rounded-full bg-neutral-700 h-10 w-10 mr-8"></div>
+                        <div className="flex-1 space-y-6 py-1">
+                            <div className="h-2 bg-neutral-700 rounded"></div>
+                            <div className="space-y-3">
+                                <div className="grid grid-cols-3 gap-4">
+                                    <div className="h-2 bg-neutral-700 rounded col-span-2"></div>
+                                    <div className="h-2 bg-neutral-700-200 rounded col-span-1"></div>
+                                </div>
+                                <div className="h-2 bg-neutral-700 rounded"></div>
+                                <div className="space-y-3 h-2 bg-neutral-700 rounded"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex space-x-4  mt-5 ">
+                        <div className="rounded-lg bg-neutral-700 h-5 w-5"></div>
+                        <div className="flex-1 space-y-6 py-1">
+                            <div className="space-y-3">
+                                <div className="grid grid-cols-3 gap-4">
+                                    <div className="h-2 bg-neutral-700 rounded col-span-2"></div>
+                                    <div className="h-2 bg-neutral-700-200 rounded col-span-1"></div>
+                                </div>
+                                <div className="h-2 bg-neutral-700 rounded"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex space-x-4  mt-5 ">
+                        <div className="rounded-lg bg-neutral-700 h-5 w-5"></div>
+                        <div className="flex-1 space-y-6 py-1">
+                            <div className="space-y-3">
+                                <div className="grid grid-cols-3 gap-4">
+                                    <div className="h-2 bg-neutral-700 rounded col-span-2"></div>
+                                    <div className="h-2 bg-neutral-700-200 rounded col-span-1"></div>
+                                </div>
+                                <div className="h-2 bg-neutral-700 rounded"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+const PoolSkeleton = () =>{
+    return(
+        <div className="animate-pulse max-w-7xl gap-12 mt-10  mx-auto shadow rounded-md">
+            <div className="  rounded-lg w-90 h-10 bg-neutral-700 ml-4">
+
+            </div>
+            <div className=" xl:flex">
+
+                <div className=" xl:w-4/12">
+
+                    <div className="space-y-3 mt-14 w-full  p-5   mb-10 lg:mb-0">
+
+                        <div className="border-4 border-neutral-700 rounded-lg">
+                            <div className=" p-4">
+                                <div className=" py-2 rounded-lg w-32 h-10 bg-neutral-700  ">
+
+                                </div>
+                                <div className=" mt-4 py-2 rounded-lg w-full h-10 bg-neutral-700  ">
+
+                                </div>
+                                <div className=" mt-4 py-2 rounded-lg w-full h-10 bg-neutral-700  ">
+
+                                </div>
+
+                                <div className="flex justify-between mt-4 py-2">
+                                    <div className="  rounded-full w-32 h-8 bg-neutral-700  ">
+
+                                    </div>
+                                    <div className=" rounded-full w-32 h-8 bg-neutral-700  ">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between mt-4 py-2">
+                                    <div className="  rounded-lg w-24 h-10 bg-neutral-700  ">
+
+                                    </div>
+                                    <div className=" rounded-lg w-24 h-10 bg-neutral-700  ">
+
+                                    </div>
+                                </div>
+
+                                <div className="flex justify-between mt-4 py-2">
+                                    <div className="  rounded-lg w-24 h-10 bg-neutral-700  ">
+
+                                    </div>
+                                    <div className=" rounded-lg w-24 h-10 bg-neutral-700  ">
+
+                                    </div>
+                                </div>
+
+                                <div className="flex justify-between mt-4 py-2">
+                                    <div className="  rounded-lg w-24 h-10 bg-neutral-700  ">
+
+                                    </div>
+                                    <div className=" rounded-lg w-24 h-10 bg-neutral-700  ">
+
+                                    </div>
+                                </div>
+
+                                <div className="flex justify-between mt-4 py-2">
+                                    <div className="  rounded-lg w-24 h-10 bg-neutral-700  ">
+
+                                    </div>
+                                    <div className=" rounded-lg w-24 h-10 bg-neutral-700  ">
+
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="xl:w-9/12">
+                    <div className="flex justify-end">
+                        <div className="px-8 py-2 rounded-lg w-32 h-10 bg-neutral-700 mr-10">
+
+                        </div>
+                        <div className="px-8 py-2 rounded-lg w-32 h-10 bg-neutral-700 ">
+
+                        </div>
+
+                    </div>
+                    <div className="flex justify-end">
+
+                        <div className=" borer-b border-white w-full h-100 mt-12 bg-neutral-700 rounded-xl">
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const DetailsSkeleton = () =>{
+    return(
+        <>
+            <div className="shadow p-5 rounded-md flex-1 space-y-6 py-1 mb-10 lg:mb-10">
+                <div className="mt-5 h-2 w-3/12 bg-neutral-700 rounded"></div>
+                <div className="space-y-3">
+                    <div className="lg:flex pb-10 lg:pb-10">
+                        <div className="h-2 w-4/12 lg:w-3/12 bg-neutral-700 rounded col-span-2 mb-5 lg:mr-32"></div>
+                        <div className="h-2 lg:w-5/12 bg-neutral-700 rounded col-span-1"></div>
+                    </div>
+                    <div className="lg:flex pb-10 lg:pb-10">
+                        <div className="h-2 w-4/12 lg:w-3/12 bg-neutral-700 rounded col-span-2 mb-5 lg:mr-32"></div>
+                        <div className="h-2 lg:w-5/12 bg-neutral-700 rounded col-span-1"></div>
+                    </div>
+                    <div className="lg:flex pb-10 lg:pb-10">
+                        <div className="h-2 w-4/12 lg:w-3/12 bg-neutral-700 rounded col-span-2 mb-5 lg:mr-32"></div>
+                        <div className="h-2 lg:w-5/12 bg-neutral-700 rounded col-span-1"></div>
+                    </div>
+                    <div className="lg:flex pb-10 lg:pb-10">
+                        <div className="h-2 w-4/12 lg:w-3/12 bg-neutral-700 rounded col-span-2 mb-5 lg:mr-32"></div>
+                        <div className="h-2 lg:w-5/12 bg-neutral-700 rounded col-span-1"></div>
+                    </div>
+                    <div className="lg:flex pb-10 lg:pb-10">
+                        <div className="h-2 w-4/12 lg:w-3/12 bg-neutral-700 rounded col-span-2 mb-5 lg:mr-32"></div>
+                        <div className="h-2 lg:w-5/12 bg-neutral-700 rounded col-span-1"></div>
+                    </div>
+                </div>
+
+            </div>
+            <div className="shadow p-5 rounded-md flex-1 space-y-6 py-1">
+
+                <div className="my-5 h-2 w-3/12 bg-neutral-700 rounded "></div>
+                <div className="mb-32 h-16   bg-neutral-700 rounded ">
+
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export {HiddenSkeleton,PoolSkeleton,DetailsSkeleton}
